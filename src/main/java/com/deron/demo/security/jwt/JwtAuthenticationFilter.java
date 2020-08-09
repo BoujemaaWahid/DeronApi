88496b0ee0f2f41ef687f189b003a27f661ba55f
Boujemaa Wahid <32971622+BoujemaaWahid@users.noreply.github.com>
2020-08-09 22:34:36 +0200

    Update JwtAuthenticationFilter.java

--- src/main/java/com/deron/demo/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/deron/demo/security/jwt/JwtAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package com.deron.demo.security.jwt;
 
 
-import com.sun.istack.internal.NotNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -15,7 +14,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
 
-    public JwtAuthenticationFilter(@NotNull  String...guards) {
+    public JwtAuthenticationFilter(String...guards) {
         super( new JwtHandler.JwtMatchers(guards) );
     }
 
